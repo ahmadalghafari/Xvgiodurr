8ea7b2e231c36140256ceee420cf1b00a73f5fa2
ghena <eng.ghena.tarboush@gmail.com>
2023-11-30 13:10:09 +0300

    i  dont know

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -92,7 +92,7 @@ class PostController extends Controller{
     }
     public function edit($id)
     {
-        $post = Post::find($id);
+        $post = post::find($id);
         return view('posts/edit', compact('post'));
     }
 
@@ -101,7 +101,7 @@ class PostController extends Controller{
         $post = Post::find($id);
         $post->text = $request->text;
         $post->save();
-        return redirect()->route('posts', $id);
+        return redirect()->route('home.posts.index');
     }
 
     public function destroy(post $post){
